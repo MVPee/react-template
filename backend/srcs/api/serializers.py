22b83ee026ceab1633d32be63db890b1bc3a6387
mvpee <mariusvanpee@hotmail.be>
2024-11-22 09:45:52 +0000

    doesn't display hashed password anymore in api

--- backend/srcs/api/serializers.py
+++ backend/srcs/api/serializers.py
@@ -3,11 +3,10 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 class UserSerializer(serializers.ModelSerializer):
-    # password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['id', 'username', 'email']
 
     def validate_password(self, value):
         if len(value) < 5:
